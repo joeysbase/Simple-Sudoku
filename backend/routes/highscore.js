32fa8497ad652e2b0648f252e2df55d9a5db0def
石钟懿 <shizhongyi@ishis-MacBook-Air.local>
2025-12-12 22:05:13 -0800

    update server address

--- backend/routes/highscore.js
+++ backend/routes/highscore.js
@@ -2,8 +2,8 @@ import express from 'express';
 import { getHighscores, postHighscore, getHighscoreByGame } from '../controllers/highscoreController.js';
 const router = express.Router();
 
-router.get('/', getHighscores);
-router.post('/', postHighscore);
-router.get('/:gameId', getHighscoreByGame);
+router.get('https://cs5610-final-wxzl.onrender.com/', getHighscores);
+router.post('https://cs5610-final-wxzl.onrender.com/', postHighscore);
+router.get('https://cs5610-final-wxzl.onrender.com/:gameId', getHighscoreByGame);
 
 export default router;
--- backend/routes/sudoku.js
+++ backend/routes/sudoku.js
@@ -2,9 +2,9 @@ import express from 'express';
 import { getGames, createGame, updateGame, deleteGame } from '../controllers/sudokuController.js';
 const router = express.Router();
 
-router.get('/', getGames);
-router.post('/', createGame);
-router.put('/:gameId', updateGame);
-router.delete('/:gameId', deleteGame);
+router.get('https://cs5610-final-wxzl.onrender.com/', getGames);
+router.post('https://cs5610-final-wxzl.onrender.com/', createGame);
+router.put('https://cs5610-final-wxzl.onrender.com/:gameId', updateGame);
+router.delete('https://cs5610-final-wxzl.onrender.com/:gameId', deleteGame);
 
 export default router;
